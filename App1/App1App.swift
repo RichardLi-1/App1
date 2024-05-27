83ed3560f44569ea8958f4dd8a2b4bf0cc5d3acd
Richard Li <79170351+RichardLi-1@users.noreply.github.com>
2024-05-27 16:49:25 -0400

    Initial Commit

---
+++ App1/App1App.swift
@@ -0,0 +1,17 @@
+//
+//  App1App.swift
+//  App1
+//
+//  Created by Richard Li on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct App1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
